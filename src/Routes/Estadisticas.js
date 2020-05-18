6a65e4d6df6c20ec24db942b32e7bba7d32c923b
Javier Estuardo Lima <32886402+JavierL999@users.noreply.github.com>
2020-05-17 23:10:40 -0600

    Update Estadisticas.js

--- src/Routes/Estadisticas.js
+++ src/Routes/Estadisticas.js
@@ -6,7 +6,7 @@ var config = require('../conexion');
 const pool = require('../database');
 
 router.get('/',async (req,res)=>{
-    const consulta = await pool.query('SELECT username, puntaje as score, fecha from partida');
+    const consulta = await pool.query('SELECT username, puntaje as score, tiempo, fecha from partida');
     res.send(consulta);
 });
 
