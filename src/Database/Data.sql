c5c936495deb1a4c3684ff749c5981041ff0919f
Javier Estuardo Lima <32886402+JavierL999@users.noreply.github.com>
2020-05-17 22:10:51 -0600

    Update Data.sql

--- src/Database/Data.sql
+++ src/Database/Data.sql
@@ -1,38 +1,19 @@
 create database p2_ace2;
 use p2_ace2;
 
-create table recorrido(
-	id_recorrido int auto_increment,
-    descripcion varchar(100),
-    distancia_recorrida varchar(100),
-    tiempo_total varchar(100),
-    fecha date,
-    primary key (id_recorrido)
+create table partida(
+	id_partida int auto_increment,
+	username varchar(100),
+	puntaje int,
+	estado int,
+	fecha date,
+	primary key(id_partida)
 );
 
-create table objeto(
-	id_objeto int auto_increment,
-    id_recorrido int not null,
-    posicion varchar(100),
-    tipo varchar(100),
-    primary key (id_objeto),
-    foreign key (id_recorrido) references recorrido(id_recorrido)
-);
-
-create table accion(
-	id_accion int auto_increment,
-    id_recorrido int not null,
-    tipo_accion varchar(20),
-    tiempo varchar(100),
-    primary key(id_accion),
-    foreign key (id_recorrido) references recorrido(id_recorrido)
-);
-
-create table velocidad(
-	id_vel int auto_increment,
-    id_recorrido int not null,
-    velocidad varchar(100),
-    tiempo varchar(100),
-    primary key (id_vel),
-    foreign key (id_recorrido) references recorrido(id_recorrido)
+create table movimiento(
+	id_mov int auto_increment,
+	tipo varchar(100),
+	id_partida int not null,
+	primary key (id_mov),
+	foreign key (id_partida) references partida(id_partida)
 );
