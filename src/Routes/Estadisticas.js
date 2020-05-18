7fe2cd92df11706256bd97f34c5606a8ea35d2a5
Javier Estuardo Lima <32886402+JavierL999@users.noreply.github.com>
2020-05-17 22:28:00 -0600

    Bitacora -> Estadisticas

---
+++ src/Routes/Estadisticas.js
@@ -0,0 +1,13 @@
+const express =  require('express');
+const router =  express.Router();
+const index = require('../index');
+var sql = require("mssql");
+var config = require('../conexion');
+const pool = require('../database');
+
+router.get('/',async (req,res)=>{
+    const consulta = await pool.query('SELECT username, puntaje as score, fecha from partida');
+    res.send(consulta);
+});
+
+module.exports=router;
