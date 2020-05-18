c5ad53f2ab03b08be34af07b1ced3eaf8bf61bfc
Javier Estuardo Lima <32886402+JavierL999@users.noreply.github.com>
2020-05-17 22:16:49 -0600

    Update Accion -> Movimiento

--- src/Routes/Accion.js
+++ src/Routes/Accion.js
@@ -7,15 +7,15 @@ const pool = require('../database');
 
 router.post('/',async (req,res)=>{
     
-    var{tipo_accion, tiempo}= req.body;
-    const consulta = await pool.query('insert into accion(id_recorrido, tipo_accion, tiempo) values ((select id_recorrido from recorrido order by id_recorrido desc limit 1),?,?);'
+    var{tipo_mov}= req.body;
+    const consulta = await pool.query('INSERT INTO movimiento (tipo, id_partida) VALUES (?,(SELECT id_partida from partida order by id_partida DESC LIMIT 1));'
     ,
-    [tipo_accion, tiempo]);
+    [tipo_accion]);
     res.send(consulta);
 });
 
 router.get('/',async (req,res)=>{
-    const consulta = await pool.query('SELECT tipo_accion, cast(avg(tiempo) as decimal(10,2)) as tiempoaccion FROM accion where id_recorrido = (select id_recorrido from recorrido order by id_recorrido DESC limit 1 ) group by(tipo_accion);');
+    const consulta = await pool.query('');
     res.send(consulta);
 })
 
